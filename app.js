07f9f4f5b10f15f83293297588365c1236bd41b3
Amit <133666068+Garzzzza@users.noreply.github.com>
2024-01-31 15:49:17 +0200

    vercel.json

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(cors({ origin: "*" }));
+app.use(cors());
 
 app.use((req, res, next) => {
   res.on("finish", () => {
